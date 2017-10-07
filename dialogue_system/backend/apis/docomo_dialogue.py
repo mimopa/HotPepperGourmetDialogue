b05d102ae8cf3efcde91bc46e68579a3893188b4
mimopa <mimopapa@gmail.com>
2017-10-07 12:28:44 +0900

    Update docomo_dialogue.py

--- dialogue_system/backend/apis/docomo_dialogue.py
+++ dialogue_system/backend/apis/docomo_dialogue.py
@@ -7,8 +7,7 @@ from doco.client import Client
 class DocomoDialogAPI(object):
 
     def __init__(self, api_key=None):
-        #api_key = os.environ.get('482f48562f66455734633370765a6e4a66624e64343553667a44383857775047732e725250643651344d35', api_key)
-        api_key = '482f48562f66455734633370765a6e4a66624e64343553667a44383857775047732e725250643651344d35'
+        api_key = 'API KEY'
         self.__client = Client(apikey=api_key)
 
     def reply(self, text):
